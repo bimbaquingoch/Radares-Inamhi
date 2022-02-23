c8ac17b08dec1c0e8b5e6459aa11ff34c4512104
Kevin Changoluisa <46755578+KevinChangoluisa@users.noreply.github.com>
2022-02-22 20:39:33 -0500

    Add files via upload

---
+++ app-front-clima/src/environments/environment.ts
@@ -0,0 +1,19 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  url: 'wss://server-socketio-kevin.herokuapp.com',
+  mapboxAccessToken:
+    'pk.eyJ1Ijoia2NoYW5nb2x1aXNhIiwiYSI6ImNreWFhYW93ZjAzaTEyd21wcml5cnpyYmQifQ.Tl4nT6_Q6FkQanBYOZ3e5A',
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
